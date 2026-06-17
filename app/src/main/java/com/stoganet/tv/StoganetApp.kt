3928106da268aefb395d71fd74259cd086d8fe6e
Aaro Koinsaari <89689072+koinsaari@users.noreply.github.com>
2026-06-17 20:44:22 +0300

    feat: configure Coil to use shared Ktor HttpClient

--- app/src/main/java/com/stoganet/tv/StoganetApp.kt
+++ app/src/main/java/com/stoganet/tv/StoganetApp.kt
@@ -1,10 +1,14 @@
 package com.stoganet.tv
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.network.ktor3.KtorNetworkFetcherFactory
 import com.stoganet.tv.di.ServiceLocator
 import timber.log.Timber
 
-class StoganetApp : Application() {
+class StoganetApp : Application(), SingletonImageLoader.Factory {
 
     lateinit var services: ServiceLocator
         private set
@@ -16,4 +20,12 @@ class StoganetApp : Application() {
         }
         services = ServiceLocator(this)
     }
+
+    @OptIn(coil3.annotation.ExperimentalCoilApi::class)
+    override fun newImageLoader(context: PlatformContext): ImageLoader =
+        ImageLoader.Builder(context)
+            .components {
+                add(KtorNetworkFetcherFactory(services.httpClient))
+            }
+            .build()
 }
